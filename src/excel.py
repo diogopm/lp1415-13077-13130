e5183db84ea2eccdc13ee58071691ce59656286b
Diogo Pina Manique <diogoprpm@gmail.com>
2014-10-13 15:41:23 +0100

    Project

---
+++ src/excel.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# authors 13077/13130
+# data: 05 de outubro de 2014
+#
+
+from mmap import mmap, ACCESS_READ
+from xlrd import open_workbook
+
+
+class Excel:
+    excelFileLoc = ""
+    workBook = ""
+    lista = [[[]]]
+    lista2 = []
+
+    def __init__(self, excelfilelocation):
+        Excel.excelFileLoc = excelfilelocation
+        print excelfilelocation
+
+    def readFile(self):
+
+        Excel.workBook = open_workbook(Excel.excelFileLoc)
+        x = 0
+        y = 0
+        for s in Excel.workBook.sheets():
+            print 'Sheet:', s.name
+            if x == 0:
+                for row in range(s.nrows):
+                    Excel.lista[x].append([])
+                    for col in range(s.ncols):
+                        Excel.lista[x][y].append(s.cell(row, col).value)
+                    y += 1
+            x += 1
+
+    def read_data(self):
+        Excel.workBook = open_workbook(Excel.excelFileLoc)
+        sheet = Excel.workBook.sheet_by_index(0)
+
+        for row in range(1, sheet.nrows):
+            lst = []
+            for col in range(1, sheet.ncols):
+                value = sheet.cell(row, col).value
+                if value != '':
+                    lst.append(value)
+                else:
+                    lst.append('null')  # não sei se é mesmo assim que se marcam os nulls
+            Excel.lista2.append(lst)
+
---
+++ src/main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# authors 13077/13130
+# data: 05 de outubro de 2014
+#
+
+from excel import Excel
+from db import Database
+
+# x = Excel("../Data/IPC_Portugal_1977_2013.xls")
+x = Excel("C:/Users/Diogo/Dropbox/ESTIG/2/S1/LP/LP-1415/IPC_Portugal_1977_2013.xls")
+#x.readFile()
+#lista = x.lista
+
+x.read_data()
+lista2 = x.lista2
+
+d = Database(lista2)
+d.drop_table()
+d.create_table()
+
+d.insert_data()
+
+d.test()
